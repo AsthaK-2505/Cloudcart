8c97e8b06906cc68afa433ef997c337bc5e4a475
Astha Kothiyal <kothiyalastha@gmail.com>
2026-09-26 09:48:41 +0530

    Add POST order routing to API gateway

--- api-gateway/app.py
+++ api-gateway/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import requests
 
 app = Flask(__name__)
@@ -8,9 +8,11 @@ USER_SERVICE = "http://user-service:5001"
 PRODUCT_SERVICE = "http://product-service:5002"
 ORDER_SERVICE = "http://order-service:5003"
 
+
 @app.route("/")
 def home():
     return "CloudCart API Gateway is running!"
+
 
 @app.route("/health")
 def health():
@@ -19,22 +21,34 @@ def health():
         "status": "healthy"
     })
 
+
 @app.route("/users")
 def users():
     response = requests.get(f"{USER_SERVICE}/users")
-    return jsonify(response.json())
+    return jsonify(response.json()), response.status_code
 
 
 @app.route("/products")
 def products():
     response = requests.get(f"{PRODUCT_SERVICE}/products")
-    return jsonify(response.json())
+    return jsonify(response.json()), response.status_code
 
 
-@app.route("/orders")
+@app.route("/orders", methods=["GET", "POST"])
 def orders():
-    response = requests.get(f"{ORDER_SERVICE}/orders")
-    return jsonify(response.json())
+
+    if request.method == "GET":
+        response = requests.get(f"{ORDER_SERVICE}/orders")
+        return jsonify(response.json()), response.status_code
+
+    data = request.get_json()
+
+    response = requests.post(
+        f"{ORDER_SERVICE}/orders",
+        json=data
+    )
+
+    return jsonify(response.json()), response.status_code
 
 
 if __name__ == "__main__":
